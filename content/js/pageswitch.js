1e6d1f8b804490efe1ea8b06090301854abe30b6
Herbert Knapp <herbert.knapp@wirecard.com>
2020-03-24 14:26:30 +0100

    Fix for interpage jumps without hash

--- content/js/pageswitch.js
+++ content/js/pageswitch.js
@@ -34,7 +34,7 @@ swup.on('contentReplaced', () => {
   refreshTitle();
   const id = window.location.hash.substr(1);
   console.log('scroll in contentReplaced to id: ' + id)
-  const pageID = window.location.pathname.replace(/.*\//,'');
+  const pageID = window.location.pathname.slice(1, -5).replace(/.*\//,'');
   const anchorElement = document.querySelectorAll('#toc_cb_' + pageID + ' + label > a')[0];
   if (anchorElement) {
     initBoxes(anchorElement);
