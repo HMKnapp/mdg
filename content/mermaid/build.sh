191b7bc01968cfb2da98e01555524ae786ec9be9
Lukas Deutz <lukas.deutz@wirecard.com>
2020-05-13 09:12:54 +0200

    Update with branch 'bugfix/mermaid'

--- content/mermaid/build.sh
+++ content/mermaid/build.sh
@@ -5,9 +5,21 @@
 
 set -euo pipefail
 
+echo
+echo "### Mermaid"
+echo
 for mmd in $(ls *.mmd | grep -v -E ".*.orig.mmd"); do
     echo "$mmd"
     mmdc -i "${mmd}" -o "${mmd%.mmd}.svg"
 done
 
 cp *.svg ../images/
+
+echo
+echo "### SVG"
+echo
+for svg in *.svg; do
+    echo "${svg}"
+    svg2png "${svg}" "${svg%.svg}.png" || echo "...failed"
+done
+cp *.png ../images/
